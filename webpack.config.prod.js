be62d18e046033917163b57fcad882290f98a7ea
Arinde Eniola <eniola.arinde@andela.co>
2016-02-27 23:53:40 +0100

    some fix

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -24,7 +24,9 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env': {
         'NODE_ENV': '"production"'
-      }
+      },
+      'global': {},
+      'global.GENTLY': false
     }),
     new webpack.optimize.UglifyJsPlugin({
       compressor: {
